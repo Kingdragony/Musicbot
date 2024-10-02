250d45cf65e74ee44f3a4b6372185623da4ec9bc
ARLODRAGON ⚡ <161493578+Kingdragony@users.noreply.github.com>
2024-10-02 21:12:46 +0300

    𝕸𝖚𝖘𝖎𝖈𝖇𝖔𝖙♫︎

---
+++ musicbot_cmds/PIng.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+zokou({ nomCom: "ping", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = 'AM ALIVE STUPID.... \n\n\nGO GITHUB AND SEARCH ANDBAD MD V1 FORK THE REPO DEPLOY AND DM FOR YOURE 15$\n\n\n';
+    let d = '                                                                           I LOVE MY OWNER';
+    let varmess = z + d;
+    var img = 'https://i.imgur.com/L4U2K76.jpeg';
+    await zk.sendMessage(dest, { video: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
