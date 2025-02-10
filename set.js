96c2dff3c8911d6702900993abab0767fec0ed9a
𝕬𝖗𝖑𝖔𝖉𝖗𝖆𝖌𝖔𝖓. <161493578+Kingdragony@users.noreply.github.com>
2025-02-10 12:12:27 +0300

    Update set.js

--- set.js
+++ set.js
@@ -1,39 +1,39 @@
-const fs = require('fs-extra');
-const { Sequelize } = require('sequelize');
-if (fs.existsSync('set.env'))
-    require('dotenv').config({ path: __dirname + '/set.env' });
+const fs = require("fs-extra");
+if (fs.existsSync("set.env")) {
+  require("dotenv").config({
+    'path': __dirname + "/set.env"
+  });
+}
 const path = require("path");
-const databasePath = path.join(__dirname, './database.db');
-const DATABASE_URL = process.env.DATABASE_URL === undefined
-    ? databasePath
-    : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
-    PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Arlodragon",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "254702713600",              
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
-    AUTO_REACT_STATUS : process.env.AUTO_REACT_STATUS || 'yes',
-    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
-    BOT : process.env.BOT_NAME || 'musicbot',
-    URL : process.env.BOT_MENU_LINKS || 'https://i.imgur.com/jQh5iIT.jpeg',
-    MODE: process.env.PUBLIC_MODE || "yes",
-    PM_PERMIT: process.env.PM_PERMIT || 'yes',
-    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
-    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
-    WARN_COUNT : process.env.WARN_COUNT || '3' ,
-    ETAT : process.env.PRESENCE || '',
-    CHATBOT : process.env.PM_CHATBOT || 'no',
-    DP : process.env.STARTING_BOT_MESSAGE || "yes",
-    ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
-    DATABASE_URL,
-    DATABASE: DATABASE_URL === databasePath
-        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
-   
+const databasePath = path.join(__dirname, "./database.db");
+const DATABASE_URL = process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL;
+module.exports = {
+  'session': process.env.SESSION_ID || "zokk",
+  'PREFIXE': process.env.PREFIX || '+',
+  'OWNER_NAME': process.env.OWNER_NAME || "𝐂.𝚵.𝚯 𝚫𝚴𝐃𝚩𝚫𝐃",
+  'NUMERO_OWNER': process.env.NUMERO_OWNER || "255783394967",
+  'AUTO_READ_STATUS': process.env.AUTO_READ_STATUS || "non",
+  'AUTO_DOWNLOAD_STATUS': process.env.AUTO_DOWNLOAD_STATUS || "non",
+  'BOT': process.env.BOT_NAME || "𝚫𝚴𝐃𝚩𝚫𝐃-𝚳D",
+  'URL': process.env.BOT_MENU_LINKS || "https://i.imgur.com/L4U2K76.jpeg",
+  'MODE': process.env.PUBLIC_MODE || 'yes',
+  'PM_PERMIT': process.env.PM_PERMIT || 'no',
+  'HEROKU_APP_NAME': process.env.HEROKU_APP_NAME,
+  'HEROKU_APY_KEY': process.env.HEROKU_APY_KEY,
+  'WARN_COUNT': process.env.WARN_COUNT || '3',
+  'ETAT': process.env.PRESENCE || '',
+  'CHATBOT': process.env.PM_CHATBOT || 'no',
+  'ANTICALL': process.env.ANTICALL || "non",
+  'AUTO_REACT_STATUS': process.env.AUTO_REACT_STATUS || "yes",
+  'DP': process.env.STARTING_BOT_MESSAGE || "yes",
+  'ADM': process.env.ANTI_DELETE_MESSAGE || "yes",
+  'DATABASE_URL': DATABASE_URL,
+  'DATABASE': DATABASE_URL === databasePath ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9"
 };
 let fichier = require.resolve(__filename);
 fs.watchFile(fichier, () => {
-    fs.unwatchFile(fichier);
-    console.log(`mise à jour ${__filename}`);
-    delete require.cache[fichier];
-    require(fichier);
+  fs.unwatchFile(fichier);
+  console.log("mise à jour " + __filename);
+  delete require.cache[fichier];
+  require(fichier);
 });
